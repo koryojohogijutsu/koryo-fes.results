0ddb9d7163ee6d25ee1734df12baceb7a3c78b9f
koryojohogijutsu <167965786+koryojohogijutsu@users.noreply.github.com>
2026-06-09 22:05:44 +0900

    Create page.tsx

---
+++ app/privacypolicy/page.tsx
@@ -0,0 +1,9 @@
+import { PrivacyPage } from "@/components/PrivacyPage";
+
+export const metadata = {
+  title: "個人情報保護への取り組みについて｜蛟龍祭クラス企画評価",
+};
+
+export default function Privacy() {
+  return <PrivacyPage />;
+}
