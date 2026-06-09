660b8ea7debd59207616d9fed7f1e35fb2ba5457
koryojohogijutsu <167965786+koryojohogijutsu@users.noreply.github.com>
2026-06-09 09:35:57 +0900

    add files

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Providers } from "./providers";
+
+export const metadata: Metadata = {
+  title: "マナビジョン ログイン",
+  description: "マナビジョンへのログイン画面です。",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect("/member");
+  } else {
+    redirect("/login");
+  }
+}
