a3858507215cb8cea39d8db0237ec9216a593dc6
koryojohogijutsu <167965786+koryojohogijutsu@users.noreply.github.com>
2026-06-09 20:47:46 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,21 @@ import "./globals.css";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-  title: "マナビジョン ログイン",
-  description: "マナビジョンへのログイン画面です。",
+  title: "蛟龍祭クラス企画評価",
+  description: "第60回蛟龍祭 クラス企画評価システム",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ja">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@900&family=Kosugi+Maru&family=Lato:wght@400;700;900&family=Noto+Sans+JP&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
