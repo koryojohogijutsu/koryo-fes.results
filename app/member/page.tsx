f067cb29ce8edcc36e89e4fce26a94023495d14a
koryojohogijutsu <167965786+koryojohogijutsu@users.noreply.github.com>
2026-06-09 09:47:44 +0900

    Update page.tsx

--- app/member/page.tsx
+++ app/member/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import { MemberPage } from "../../components/MemberPage";
+import { MemberPage } from "@/components/MemberPage";
 
 export default async function Member() {
   const session = await getServerSession(authOptions);
