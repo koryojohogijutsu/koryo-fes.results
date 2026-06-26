aefde21a4c4a31f2133206574e5429fe78adffd7
koryojohogijutsu <167965786+koryojohogijutsu@users.noreply.github.com>
2026-06-27 08:30:39 +0900

    Update LogoutButton.tsx

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,11 +1,6 @@
 "use client";
 import { signOut } from "next-auth/react";
 import styles from "./MemberPage.module.css";
-
 export function LogoutButton() {
-  return (
-    <button className={styles.logoutBtn} onClick={() => signOut({ callbackUrl:"/login" })}>
-      ログアウト
-    </button>
-  );
+  return <button className={styles.logoutBtn} onClick={() => signOut({ callbackUrl:"/login" })}>ログアウト</button>;
 }
